4ed8da47fa748263199a04cb548fcf901bd6a912
Nilayan17 <mazumdarnilayan@gmail.com>
2025-01-10 13:37:41 +0530

    Displaying all contents of queue

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const { parse } = require('dotenv');
 const express = require('express')
 const {createClient}=require('redis')
 
@@ -18,19 +19,24 @@ app.post('/test',async(req,res)=>{
     try {
     const data =req.body;
     await client.connect();
-    
-    await client.lPush('submissions',JSON.stringify(data));
-     
-    
+    await client.lPush('submissions',JSON.stringify(data));    
     res.status(200).json({message:"success"})
     } catch (err) {
-        console.log(err)
-    }
-
-   
-
+        console.log(err);
+    }  
 })
 
+app.get('/tasks', async(req, res) => {
+  try {
+    await client.connect();
+    const tasks = await client.lRange('submissions',0,-1);
+    const parsedTasks = tasks.map(task => JSON.parse(task));
+    console.log('Tasks:', parsedTasks);
+    res.status(200).json(parsedTasks);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
