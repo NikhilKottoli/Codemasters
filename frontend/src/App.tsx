a8479826e18ea417fe0f42656ef85c4f5cb363f6
dineshmanideep <dineshmanideep@gmail.com>
2025-01-19 14:54:21 +0530

    added home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/user/signin" element={<SignIn />} />
         <Route path="/user/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Toaster />
     </Router>
